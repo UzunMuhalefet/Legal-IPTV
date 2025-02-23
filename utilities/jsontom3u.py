f3efeb38e0b64c404f31f34c6a906dfdc2520038
Ramazan Sancar <ramazansancar4545@gmail.com>
2025-02-23 15:59:49 +0300

    update: Updated data fix: slugify fixed.

--- utilities/jsontom3u.py
+++ utilities/jsontom3u.py
@@ -1,5 +1,19 @@
 import os
-from slugify import slugify
+import re, unicodedata
+
+def slugify(deger:str, unicode_izin=False) -> str:
+    """
+    'unicode_izin' False ise ASCII'ye dönüştürür.
+    Boşlukları veya tekrarlanan kısa çizgileri tek tirelere dönüştürür.
+    Alfasayısal, alt çizgi veya kısa çizgi olmayan karakterleri kaldırıp Küçük harfe dönüştür.
+    Ayrıca baştaki ve sondaki beyaz boşlukları, tireleri ve alt çizgileri de çıkarır.
+    """
+    if unicode_izin:
+        deger = unicodedata.normalize("NFKC", deger)
+    else:
+        deger = unicodedata.normalize("NFKD", deger).encode("ascii", "ignore").decode("ascii")
+    deger = re.sub(r"[^\w\s-]", "", deger.lower())
+    return re.sub(r"[-\s]+", "-", deger).strip("-_")
 
 current_dir = os.getcwd()
 
